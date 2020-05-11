4cb6278ccacbf87db63a82f27920bc984e3937fa
Roopesh-g <gujarathi.roopesh@gmail.com>
2020-05-11 17:21:15 +0100

    created to-do react app

---
+++ projects/to-do/src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const content = (
+    <div>
+        <input type="checkbox" />
+        <p>Place holder text here</p>
+
+        <input type="checkbox" />
+        <p>Place holder text here</p>
+
+        <input type="checkbox" />
+        <p>Place holder text here</p>
+
+        <input type="checkbox" />
+        <p>Place holder text here</p>
+
+    </div>
+)
+class App extends React.Component {
+    render() {
+        return content    
+    }
+}
+
+export default App
